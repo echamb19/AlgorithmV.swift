bbe85606dafcb9a2587e5ec2414f28545958647a
Eden Chambers <EdenChambers5@gmail.com>
2018-10-23 12:47:07 -0600

    I added my steps for the Algorithm. I also added the outlineStyle thing.

--- AlgorithmV.swift/Controller/AlgorithmViewController.swift
+++ AlgorithmV.swift/Controller/AlgorithmViewController.swift
@@ -8,20 +8,42 @@
 
 import UIKit
 
-class AlgorithmViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+public class AlgorithmViewController: UIViewController
+{
+   
     @IBOutlet weak var UIImageView1: UIImageView!
     
     @IBOutlet weak var Label1: UILabel!
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        formatAlgorithm()
     }
-
+   
+   private func formatAlgorithm() -> Void
+    {
+        let title : String = "How to Make an App in XCode"
+        
+        let stepOne : String = "Open XCode, and select ‘Create New Xcode Project’. Hit next leaving the first page with the default settings. Name your Project with no spaces and ending with ‘.swift’, and select the previously made folder to create it in. "
+        let stepTwo : String = "Right click your project folder in the Navigation pane, and make four groups nestled inside the Project’s main folder. (Controller, View, Model, and Resources)"
+        let stepThree : String = "Put the Main.storyboard and the LaunchScreen.storyboard inside the View folder. The ViewController.swift should be put inside the Controller folder. The Assets.xcassets, AppDelegate.swift, and Info.plist segments should be put inside the Resources folder."
+        let stepFour : String = "Click on your App’s name in the Navigation pane (Not the folder). In the identity section, click ‘Choose Info.plist File…’. Select your Info.plist file inside your resources folder in the popup screen. Click Main.storyboard in the Navigation pane, then click the project Name again. The identity section should not have the choice to choose an Info.plist file anymore."
+        let stepFive : String = "Made a new .swift file in the Model folder and name it something relevant."
+    }
+    
+    private func createOutlineStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
+    }
 
 }
 
